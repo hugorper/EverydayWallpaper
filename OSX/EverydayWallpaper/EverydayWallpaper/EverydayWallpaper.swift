34446ef4726e8c9b7202e9dd4b31b2c4fe23161b
Hugo PEREIRA <hugorper@gmail.com>
2015-11-16 19:39:39 +0100

    chore(OS X): add base preference pane app

---
+++ OSX/EverydayWallpaper/EverydayWallpaper/EverydayWallpaper.swift
@@ -0,0 +1,19 @@
+//
+//  EverydayWallpaper.swift
+//  EverydayWallpaper
+//
+//  Created by Hugo PEREIRA on 16.11.15.
+//  Copyright © 2015 Hugo PEREIRA. All rights reserved.
+//
+
+import Foundation
+import PreferencePanes
+
+class EverydayWallpaper: NSPreferencePane {
+    
+    override func mainViewDidLoad() {
+        
+    }
+    
+
+}

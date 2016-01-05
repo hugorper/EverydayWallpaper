9b716d7e86f90d96dc04b54dc8c9213faa91a3ba
Hugo PEREIRA <hugorper@gmail.com>
2016-01-05 20:41:16 +0100

    refactor(screen): remove unused code

--- OSX/EverydayWallpaperBar/EverydayWallpaperBar/ScreenInfo.swift
+++ OSX/EverydayWallpaperBar/EverydayWallpaperBar/ScreenInfo.swift
@@ -14,11 +14,11 @@ class ScreenInfo {
     static internal func screensCount() -> Int {
         return (NSScreen.screens()?.count)!
     }
-
+    
     static internal func mainScreenSize() -> CGSize {
+        // screen index 0 is always the main screen
         return self.screensSizeFromIndex(0)
     }
-
     
     static internal func screensSizeFromIndex(zeroBasedIndex: Int) -> CGSize {
         let screen: NSScreen = NSScreen.screens()![zeroBasedIndex]
@@ -26,23 +26,5 @@ class ScreenInfo {
         let displayPixelSize: NSSize = description.objectForKey(NSDeviceSize)!.sizeValue
         
         return CGSizeMake(displayPixelSize.width, displayPixelSize.height)
-    }
-    
-    
-    
-    static internal func makeSizeFromCentimeters(width: CGFloat, height: CGFloat) -> CGSize {
-        
-        let screen: NSScreen = NSScreen.mainScreen()!
-        
-        let description: NSDictionary = screen.deviceDescription
-        let displayPixelSize: NSSize = description.objectForKey(NSDeviceSize)!.sizeValue
-        let displayPhysicalSize: CGSize = CGDisplayScreenSize(description.objectForKey("NSScreenNumber")!.unsignedIntValue)
-        let resolution = (displayPixelSize.width / displayPhysicalSize.width) * 25.4
-        
-        let pixelsWidth: CGFloat = 0.394 * width * resolution
-        let pixelsHeight: CGFloat = 0.394 * height * resolution
-        
-        return CGSizeMake(pixelsWidth, pixelsHeight)
-        
     }
 }

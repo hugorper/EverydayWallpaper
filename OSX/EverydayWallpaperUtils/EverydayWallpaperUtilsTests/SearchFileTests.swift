e3f9e50be4ebf7f80adcbc5b7f72febd0d182816
Hugo PEREIRA <hugorper@gmail.com>
2016-03-22 00:09:47 +0100

    test(wallpaper): validate SearchFile.listOfFileToDelete()
    Complet test if only desired files are returned from list of file to delete

--- OSX/EverydayWallpaperUtils/EverydayWallpaperUtilsTests/SearchFileTests.swift
+++ OSX/EverydayWallpaperUtils/EverydayWallpaperUtilsTests/SearchFileTests.swift
@@ -75,4 +75,223 @@ class SearchFileTests: XCTestCase
         }
     }
 
+    func testKeep0Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 0).count == 10, "Keep 0 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep1Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 1).count == 8, "Keep 1 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep2Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 2).count == 7, "Keep 2 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep3Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 3).count == 6, "Keep 3 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep4Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 4).count == 5, "Keep 4 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep5Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 5).count == 1, "Keep 5 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep6Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 6).count == 0, "Keep 6 day fail")
+
+        deleteAllFilesHelper()
+    }
+
+    func testKeep99Day()
+    {
+        self.deleteAllFilesHelper()
+
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-19 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-13 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-03-03 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-27 06:00:00"), market: BingWallperMarkets.JapaneseJapan.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishAustralia.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishCanada.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishNewZealand.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2016-02-24 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+        createFakeWallpaperFileHelper(NSDate(dateString: "2000-12-15 06:00:00"), market: BingWallperMarkets.EnglishUnitedKingdom.rawValue)
+
+        XCTAssert(SearchFile.listOfFileToDelete(ImageDownloader.sharedLoader.WallpaperSavePath, keepFileCount: 99).count == 0, "Keep 99 day fail")
+
+        deleteAllFilesHelper()
+    }
+    // Utility function
+    func createFakeWallpaperFileHelper(date: NSDate, market: String)
+    {
+        let naming = WallpaperFiles.init(provider: WallpaperFiles.BingProvider, withBaseFolder: ImageDownloader.sharedLoader.WallpaperSavePath, withSize: CGSizeMake(1024, 768), withDate: date, withMarket: market)
+
+        let content = "for exist test file"
+
+        let fileName = naming.fullName(date)
+        content.toFile(fileName)
+    }
+
+    // Utility function
+    func deleteAllFilesHelper()
+    {
+        let keys = [NSURLIsDirectoryKey, NSURLLocalizedNameKey]
+        let validateWallpaper = "\(Constants.Naming.FileNameSeparator)\(Constants.Naming.Bing)\(Constants.Naming.FileNameSeparator)"
+
+        do
+        {
+            let directoryUrls = try NSFileManager.defaultManager().contentsOfDirectoryAtURL(NSURL(fileURLWithPath: ImageDownloader.sharedLoader.WallpaperSavePath), includingPropertiesForKeys: keys, options: .SkipsHiddenFiles)
+
+            let urls = directoryUrls.filter
+            {
+                $0.pathExtension == "jpg"
+            }.map
+            {
+                $0
+            }.sort({ $0.lastPathComponent > $1.lastPathComponent })
+
+
+            for url in urls
+            {
+                do
+                {
+                    let resourceValues = try url.resourceValuesForKeys(keys)
+
+                    if let isDirectory = resourceValues[NSURLIsDirectoryKey] as? NSNumber
+                    {
+                        if !isDirectory.boolValue
+                        {
+                            if url.lastPathComponent!.rangeOfString(validateWallpaper) != nil
+                            {
+                                if url.lastPathComponent![0 ... 7].isValidShortDateString()
+                                {
+                                    try NSFileManager.defaultManager().removeItemAtURL(url)
+                                }
+                            }
+                        }
+                    }
+
+                }
+                catch
+                {
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
 }
